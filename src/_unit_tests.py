b957b72125422b52f4613a4f1eb36934021111f3
Thomas Lacasse <thomas@kukt.us>
2020-04-10 14:38:57 -0400

    holstep tree parser

---
+++ src/_unit_tests.py
@@ -0,0 +1,110 @@
+import unittest
+from holstep import HolstepTreeParser as HTP, QuickHolstepSeqParser as QHSP
+
+parser = HTP()
+seqparser = QHSP()
+
+def check(test, expr, expected, exp_vars=[], exp_varfuncs=[]):
+    tree = parser.parse(expr)
+    tree.build_unique_info()
+
+    check_node(test, tree, expected)
+    test.assertFalse(tree.has_FILL())
+    test.assertFalse(any([b for b in tree.unique_branching.keys() if b > 2]))
+    test.assertEqual(len(seqparser.parse(expr)), tree.node_count())
+    
+    test.assertEqual(len(parser.stack), 1)
+    test.assertEqual(len(parser.varlist), len(exp_vars))
+    for a, e in zip(parser.varlist, exp_vars):
+        test.assertEqual(a, e)
+    test.assertEqual(len(parser.varfunclist), len(exp_varfuncs))
+    for a, e in zip(parser.varfunclist, exp_varfuncs):
+        test.assertEqual(a, e)
+    
+def check_node(test, node, expected):
+    print('"{}" =?= "{}"'.format(node.value, expected[0]))
+    test.assertEqual(node.value, expected[0])
+    test.assertTrue(len(node.children) <= 2)
+    test.assertEqual(len(node.children), len(expected[1:]))
+    for n, e in zip(node.children, expected[1:]):
+        check_node(test, n, e)
+
+class TestHolstepTreeParsing(unittest.TestCase):
+
+    def test_basic_parsing(self):
+        check(self, '|- (!n. (!m. (((SUC m) - (SUC n)) = (m - n))))',
+              ['|-', 
+               ['!', ['n', ['.']],
+                 ['!', ['m', ['.']],
+                   ['=',
+                    ['-',
+                     ['SUC', ['m']],
+                     ['SUC', ['n']]
+                     ],
+                    ['-', ['m'], ['n']]
+                    ]]]], exp_vars=['n', 'm'])
+    
+    
+    def test_predicates_and_negation(self):
+        check(self, '|- (!P. ((~ (?x. (P x))) = (!x. (~ (P x)))))',
+              ['|-',
+               ['!', ['P', ['.']],
+                ['=',
+                 ['~', 
+                  ['?', ['x', ['.']],
+                   ['P', ['x']]
+                  ],
+                 ], 
+                 ['!', ['x', ['.']],
+                  ['~', 
+                   ['P', ['x']]
+                   ]]]]], exp_vars=['x'], exp_varfuncs=['P'])
+    
+    def test_predicate_var_and_func(self):
+        check(self, '|- (!P. (!Q. ((P \/ (!x. (Q x))) = (!x. (P \/ (Q x))))))',
+              ['|-',
+               ['!', ['P', ['.']],
+                ['!', ['Q', ['.']],
+                 ['=',
+                  ['\\/', 
+                   ['P'],
+                   ['!', ['x', ['.']], ['Q', ['x']]]
+                  ],
+                  ['!', ['x', ['.']],
+                   ['\\/', ['P'], ['Q', ['x']]]
+                   ]]]]], exp_vars=['x'], exp_varfuncs=['P', 'Q'])
+    
+    def test_T_F_and_non_quantified_vars(self):
+        check(self, '|- ((!x. (P x)) = ((P T) /\ (P F)))',
+              ['|-',
+               ['=',
+                ['!', ['x', ['.']], ['P', ['x']]],
+                ['/\\',
+                 ['P', ['T']],
+                 ['P', ['F']]
+                 ]]]
+               , exp_vars=['x'], exp_varfuncs=['P'])
+
+    def test_deep_operation(self):
+        check(self, '|- (!x. (!y. ((NUMERAL (BIT1 _0)) + (NUMERAL (BIT1 _0)))))',
+              ['|-', 
+               ['!', ['x', ['.']],
+                 ['!', ['y', ['.']],
+                   ['+',
+                    ['NUMERAL', ['BIT1', ['_0']]],
+                    ['NUMERAL', ['BIT1', ['_0']]]
+                ]]]], exp_vars=['x', 'y'])
+            
+    def test_chained_funcs(self):
+        check(self, '|- (((real_div x) (real_of_num (NUMERAL (BIT1 _0)))) = x)',
+              ['|-', 
+               ['=',
+                ['real_div', 
+                 ['x'],
+                 ['real_of_num', ['NUMERAL', ['BIT1', ['_0']]]]
+                ],
+                ['x']
+                ]], exp_vars=['x'])
+
+if __name__ == '__main__':
+    unittest.main()
--- src/holstep.py
+++ src/holstep.py
@@ -1,5 +1,6 @@
 import re
 from data import DatabaseAccess
+from collections import Counter
 
 class Holstep(DatabaseAccess):
     
@@ -41,6 +42,15 @@ class HolstepToken:
         
     def __repr__(self):
         return '<"{}" : "{}">'.format(self.token, self.kind)
+    
+    def __str__(self):
+        return repr(self)
+    
+    def __eq__(self, other):
+        return self.token == other.token and self.kind == other.kind
+    
+    def __hash__(self):
+        return hash((self.token, self.kind))
 
 # parse through "text" column
 class HolstepParser:
@@ -146,5 +156,208 @@ class HolstepTokenizer:
                 return 'SYM'
     
     def parse(self, tokens):
-        tokens = tokens.split(' ')
+        
         return [HolstepToken(t, self.get_token_kind(t)) for t in tokens]
+
+class HolstepTreeNode:
+    
+    def __init__(self, token):
+        self.settoken(token)
+        self.children = []
+        self.unique_tokens = Counter()
+        self.unique_branching = Counter()
+        
+    def settoken(self, token):
+        self.token = token
+        self.value = token.token
+        
+    def consumefirstchild(self):
+        first = self.children[0]
+        tail = self.children[1:]
+        self.settoken(first.token)
+        self.children = first.children + tail
+        
+    def printtree(self, indent=0):
+        print(('   ' * indent) + self.value)
+        for c in self.children:
+            c.printtree(indent=indent+1)
+            
+    def to_list(self):
+        result = []
+        result.append(self.value)
+        for c in self.children:
+            result += c.to_list()
+        return result
+            
+    def build_unique_info(self):
+        for c in self.children:
+            c.build_unique_info()
+            self.unique_tokens.update(c.unique_tokens)
+            self.unique_branching.update(c.unique_branching)
+        self.unique_tokens.update([self.token])
+        self.unique_branching.update([len(self.children)])
+        
+    def has_FILL(self):
+        for c in self.children:
+            if (c.has_FILL()):
+                return True
+        return self.value == 'FILL'
+    
+    def node_count(self, is_inner=False):
+        count = 0 if is_inner else 1
+        for c in self.children:
+            count += 1 + c.node_count(is_inner=True)
+        return count
+            
+    def __repr__(self):
+        return '[{}, {}]'.format(self.value, len(self.children))
+    
+    def __str__(self):
+        return repr(self)
+    
+class QuickHolstepSeqParser:
+    
+    def __init__(self):
+        pass
+    
+    def parse(self, tokens):
+        tokensspl = tokens.split(' ')
+        tokens = []
+        for token in tokensspl:
+            token = token.lstrip('(').rstrip(')')
+            if len(token) >= 2 and token[1].isalpha() and not token[0].isalpha():
+                tokens.append(token[0])
+                tokens.append(token[1])
+                token = token[2:]
+                if token != '':
+                    tokens.append(token)
+            else:
+                tokens.append(token)
+        return tokens
+    
+class HolstepTreeParser:
+    
+    def __init__(self):
+        self.varlist = []
+        self.varfunclist = []
+        self.stack = []
+        self.latest_source = None
+        self.prevtoken = None
+        
+    def parse(self, tokens):
+        self.latest_source = tokens
+        tokens = tokens.split(' ')
+        self.varlist = []
+        self.varfunclist = []
+        
+        root = HolstepTreeNode(HolstepToken('ROOT', 'ROOT'))
+        self.stack = [root]
+        
+        for token in tokens:
+            if token == '|-':
+                self._handle_fun('|-')
+            elif token[0] == '(':
+                self._handle_beginning_paren(token)
+            elif self.is_word(token):
+                self._handle_word(token)
+            else:
+                self._handle_operation(token)
+            self.prevtoken = token
+        return root
+
+    def _handle_beginning_paren(self, token):
+        # multiple '((((('
+        while token[0] == '(':
+            node = HolstepTreeNode(HolstepToken('FILL', 'FILL'))
+            self.stack[-1].children.append(node)
+            self.stack.append(node)
+            token = token[1:]
+        if self.is_word(token):
+            # funcs or constants or vars
+            self._handle_word(token)
+        else:
+            if len(token) > 1 and token[1].isalpha():
+                # quantifiers
+                if (len(token) == 2 or token[2].isalpha()):
+                    raise Exception(token)
+                self._handle_quantifier(token)
+            else:
+                # symbol func like '~'
+                self._handle_fun(token)
+                
+    def _handle_quantifier(self, token):
+        qnt = token[0]
+        token = token[1:]
+        self._handle_fun(qnt, kind='QNT')
+        var = token[0]
+        syms = token[1:]
+        self._handle_var(var, syms=syms)
+        
+    def _handle_word(self, token):
+        token, parens = self.split_end_parens(token)
+        #self._handle_higher_order_fun(token)
+        if self.prevtoken.lstrip('(') in self.varfunclist:
+            # predicate on var
+            self.stack[-1].consumefirstchild()
+        if token in self.varlist:
+            # var
+            self._handle_var(token)
+        elif token in self.varfunclist:
+            # predicate
+            self._handle_var(token)
+            #self._handle_fun(token, kind='VFN')
+        elif token[0] == '_' or token in ['T', 'F']:
+            # constant ex: '_0' or 'T'
+            self._handle_value(token)
+        else:
+            if len(token) == 1:
+                # assume var
+                self._handle_var(token)
+            else:
+                # function
+                self._handle_fun(token)
+        if parens is not None:
+            for i in range(len(parens)):
+                if self.stack[-1].value == 'FILL':
+                    self.stack[-1].consumefirstchild()
+                self.stack.pop()
+                
+    def _handle_operation(self, token):
+        self._handle_fun(token, kind='OPR')
+            
+    def _handle_fun(self, token, kind='FUN'):
+        self.stack[-1].settoken(HolstepToken(token, kind))
+        
+    def _handle_var(self, var, syms=None):
+        varnode = None
+        if var.islower():
+            if var not in self.varlist:
+                self.varlist.append(var)
+            varnode = HolstepTreeNode(HolstepToken(var, 'VAR'))
+        else:
+            if var not in self.varfunclist:
+                self.varfunclist.append(var)
+            varnode = HolstepTreeNode(HolstepToken(var, 'VFN'))
+        qnt = self.stack[-1]
+        qnt.children.append(varnode)
+        if syms is not None:
+            symsnode = HolstepTreeNode(HolstepToken(syms, 'DOT'))
+            varnode.children.append(symsnode)
+  
+    def _handle_value(self, token):
+        node = HolstepTreeNode(HolstepToken(token, 'VAL'))
+        self.stack[-1].children.append(node)
+        
+    def _handle_higher_order_fun(self, token):
+        if self.stack[-1].token.kind == 'FILL' and self.prevtoken[-1] == ')':
+            self._consume_child(self.stack[-1])
+        
+    def is_word(self, token):
+        return token[0].isalpha() or token[0] == '_'
+    
+    def split_end_parens(self, token):
+        i = token.find(')')
+        if i == -1:
+            return (token, None)
+        else:
+            return (token[:i], token[i:])
